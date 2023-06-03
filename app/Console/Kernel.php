c47ca079b67e5ef292b4e15964fbdbd5b1ba870c
Olivier <o.ettlin@gmail.com>
2023-06-03 19:36:57 +0200

    Schedule script movies

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:store-trending-day-movies')->daily();
+        $schedule->command('app:store-week-day-movies')->daily();
     }
 
     /**
