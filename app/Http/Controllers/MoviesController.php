fd400100215a7df797f85dfdca70e6e481429b3f
Olivier <o.ettlin@gmail.com>
2023-06-03 17:04:01 +0200

    Template movies

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Services\TMDBService;
+use App\Models\Movie;
+use App\Models\TrendingMovie;
 
 class MoviesController extends Controller
 {
@@ -11,12 +12,31 @@ class MoviesController extends Controller
      * Invoke the controller method.
      *
      * @param  Request  $request
-     * @param  TMDBService $tmdbService
      * @return mixed
      */
-    public function __invoke(Request $request, TMDBService $tmdbService)
+    public function __invoke(Request $request)
     {
-        $popularMovies = $tmdbService->getTrendingWeekMovies()();
-        return view('movies.movies');
+        $trendingMovies = TrendingMovie::where('period', 'day')->get();
+        $dayMovies = [];
+
+        foreach ($trendingMovies as $trendingMovie) {
+            $movie = Movie::find($trendingMovie->id_movie);
+            if ($movie instanceOf Movie) {
+                $dayMovies[] = $movie;
+            }
+        }
+
+        $trendingMovies = TrendingMovie::where('period', 'week')->get();
+        $weekMovies = [];
+
+        foreach ($trendingMovies as $trendingMovie) {
+            $movie = Movie::find($trendingMovie->id_movie);
+            if ($movie instanceOf Movie) {
+                $weekMovies[] = $movie;
+            }
+        }
+
+        return view('movies.movies', compact('dayMovies', 'weekMovies'));
+
     }
 }
--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -4,4 +4,31 @@
             Films populaires
         </h2>
     </x-slot>
+
+    <div id="movies" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="mb-6 mt-6 ml-6 md-6">
+            <h2 class="text-2xl font-bold mb-4">Tendances du jour</h2>
+            @foreach ($dayMovies as $movie)
+                <div class="bg-white shadow-lg rounded-lg">
+                    <h2 class="text-xl font-bold mt-4 ml-4 md-4">{{ $movie->title }}</h2>
+                    <p class="text-gray-600  mb-4 ml-4 md-4">Date de sortie: {{ \Carbon\Carbon::parse($movie->release_date)->format('d/m/Y') }}</p>
+                    <img src="{{ $movie->getPosterUrlAttribute() }}" alt="{{ $movie->title }}" class="w-full h-64 object-cover mt-4">
+                    <p class="text-gray-600  mb-4 mt-4 ml-4 md-4">{{ $movie->overview }}</p>
+                </div>
+            @endforeach
+        </div>
+
+        <div class="mb-6 mt-6 ml-6 md-6">
+            <h2 class="text-2xl font-bold mb-4">Tendances de la semaine</h2>
+            @foreach ($weekMovies as $movie)
+                <div class="bg-white shadow-lg rounded-lg">
+                    <h2 class="text-xl font-bold mt-4 ml-4 md-4">{{ $movie->title }}</h2>
+                    <p class="text-gray-600  mb-4 ml-4 md-4">Date de sortie: {{ \Carbon\Carbon::parse($movie->release_date)->format('d/m/Y') }}</p>
+                    <img src="{{ $movie->getPosterUrlAttribute() }}" alt="{{ $movie->title }}" class="w-full h-64 object-cover mt-4">
+                    <p class="text-gray-600  mb-4 mt-4 ml-4 md-4">{{ $movie->overview }}</p>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    
 </x-app-layout>
